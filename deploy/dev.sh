1d4a4cfcc3ed6c238abfe1fbd5418c83d81c449e
phenix3443 <phenix3443@gmail.com>
2024-08-14 15:27:45 +0800

    feat: some fixes

--- deploy/dev.sh
+++ deploy/dev.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 basedir=$(
-	cd $(dirname "$0") || exit
-	pwd
+    cd $(dirname "$0") || exit
+    pwd
 )
 
-echo ${basedir}
+echo "${basedir}"
 
 project_path=${basedir}/../
 bin=${project_path}/build/bdjuno
@@ -23,18 +23,15 @@ build)
 init)
     echo "===== init ===="
 
-
     echo "===== end ===="
     ;;
 start)
     echo "===== start ===="
 
-
     echo "===== end ===="
     ;;
 stop)
     echo "===== stop ===="
-
 
     echo "===== end ===="
     ;;
@@ -43,25 +40,24 @@ reset)
 
     killall bdjuno
 
-    cd ${project_path}
+    cd "${project_path}" || exit
     make build
 
-    cd ${project_path}/deploy
-    docker-compose -f docker-compose.yaml down
+    cd "${project_path}"/deploy || exit
+    docker compose -f docker-compose.yaml down
     rm -rf ./data
-    docker-compose -f docker-compose.yaml up -d
+    docker compose -f docker-compose.yaml up -d
 
     echo "wait 45s for graphql engine start..."
-    for ((i=45; i>0; i-=3))
-    do
+    for ((i = 90; i > 0; i -= 3)); do
         echo "please wait ${i}s..."
         sleep 3
     done
 
-    cd $project_path/hasura
+    cd "$project_path"/hasura || exit
     hasura metadata apply --endpoint http://localhost:8080 --admin-secret 123456
 
-    cd $project_path/deploy
+    cd "$project_path"/deploy || exit
     cp config.yaml ./data
     ${bin} parse genesis-file --genesis-file-path ./genesis.json --home ./data
     #nohup ${bin} start --home ./data >./bdjuno.log 2>&1 &
