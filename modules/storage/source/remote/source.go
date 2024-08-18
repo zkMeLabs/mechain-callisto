0ca53fdc44dd2e7a2e39b20f28bc019f52b375b3
Luke <luchenqun@qq.com>
2024-08-18 21:30:55 +0800

    feat: imp some interface for remote storage

--- modules/storage/source/remote/source.go
+++ modules/storage/source/remote/source.go
@@ -1,7 +1,9 @@
 package remote
 
 import (
+	permission "github.com/forbole/bdjuno/v4/modules/storage/permission"
 	types "github.com/forbole/bdjuno/v4/modules/storage/types"
+	vitualgroup "github.com/forbole/bdjuno/v4/modules/storage/vitualgroup"
 	"github.com/forbole/juno/v5/node/remote"
 
 	storagesource "github.com/forbole/bdjuno/v4/modules/storage/source"
@@ -35,5 +37,96 @@ func (s Source) HeadBucket(height int64, bucketName string) (types.BucketInfo, t
 		return types.BucketInfo{}, types.BucketExtraInfo{}, err
 	}
 
-	return *res.BucketInfo, *res.ExtraInfo, err
+	return *res.BucketInfo, *res.ExtraInfo, nil
+}
+
+func (s Source) HeadBucketById(height int64, bucketId string) (types.BucketInfo, types.BucketExtraInfo, error) {
+	res, err := s.storageClient.HeadBucketById(
+		remote.GetHeightRequestContext(s.Ctx, height),
+		&types.QueryHeadBucketByIdRequest{BucketId: bucketId},
+	)
+	if err != nil {
+		return types.BucketInfo{}, types.BucketExtraInfo{}, err
+	}
+
+	return *res.BucketInfo, *res.ExtraInfo, nil
+}
+
+func (s Source) HeadBucketExtra(height int64, bucketName string) (types.InternalBucketInfo, error) {
+	res, err := s.storageClient.HeadBucketExtra(
+		remote.GetHeightRequestContext(s.Ctx, height),
+		&types.QueryHeadBucketExtraRequest{BucketName: bucketName},
+	)
+	if err != nil {
+		return types.InternalBucketInfo{}, err
+	}
+
+	return *res.ExtraInfo, nil
+}
+
+func (s Source) HeadGroup(height int64, groupOwner, groupName string) (types.GroupInfo, error) {
+	res, err := s.storageClient.HeadGroup(
+		remote.GetHeightRequestContext(s.Ctx, height),
+		&types.QueryHeadGroupRequest{
+			GroupOwner: groupOwner,
+			GroupName:  groupName,
+		},
+	)
+	if err != nil {
+		return types.GroupInfo{}, err
+	}
+
+	return *res.GroupInfo, nil
+}
+
+func (s Source) HeadGroupMember(height int64, member, groupOwner, groupName string) (permission.GroupMember, error) {
+	res, err := s.storageClient.HeadGroupMember(
+		remote.GetHeightRequestContext(s.Ctx, height),
+		&types.QueryHeadGroupMemberRequest{
+			Member:     member,
+			GroupOwner: groupOwner,
+			GroupName:  groupName,
+		},
+	)
+	if err != nil {
+		return permission.GroupMember{}, err
+	}
+
+	return *res.GroupMember, nil
+}
+
+func (s Source) HeadObject(height int64, bucketName, objectName string) (types.ObjectInfo, vitualgroup.GlobalVirtualGroup, error) {
+	res, err := s.storageClient.HeadObject(
+		remote.GetHeightRequestContext(s.Ctx, height),
+		&types.QueryHeadObjectRequest{BucketName: bucketName, ObjectName: objectName},
+	)
+	if err != nil {
+		return types.ObjectInfo{}, vitualgroup.GlobalVirtualGroup{}, err
+	}
+
+	return *res.ObjectInfo, *res.GlobalVirtualGroup, nil
+}
+
+func (s Source) HeadObjectById(height int64, objectId string) (types.ObjectInfo, vitualgroup.GlobalVirtualGroup, error) {
+	res, err := s.storageClient.HeadObjectById(
+		remote.GetHeightRequestContext(s.Ctx, height),
+		&types.QueryHeadObjectByIdRequest{ObjectId: objectId},
+	)
+	if err != nil {
+		return types.ObjectInfo{}, vitualgroup.GlobalVirtualGroup{}, err
+	}
+
+	return *res.ObjectInfo, *res.GlobalVirtualGroup, nil
+}
+
+func (s Source) HeadShadowObject(height int64, bucketName, objectName string) (types.ShadowObjectInfo, error) {
+	res, err := s.storageClient.HeadShadowObject(
+		remote.GetHeightRequestContext(s.Ctx, height),
+		&types.QueryHeadShadowObjectRequest{BucketName: bucketName, ObjectName: objectName},
+	)
+	if err != nil {
+		return types.ShadowObjectInfo{}, err
+	}
+
+	return *res.ObjectInfo, nil
 }
