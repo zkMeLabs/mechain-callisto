5e9ed5d885edd4d283914b70141ae75f2405052f
phenix3443 <phenix3443@gmail.com>
2024-08-23 14:03:11 +0800

    feat: fix bug

--- database/object.go
+++ database/object.go
@@ -18,7 +18,7 @@ func (db *DB) UpdateObject(ctx context.Context, object *models.Object) error {
 
 func (db *DB) GetObject(ctx context.Context, objectId string) (*models.Object, error) {
 	var object models.Object
-	if err := db.G.WithContext(ctx).Table((&models.Bucket{}).TableName()).Where(
+	if err := db.G.WithContext(ctx).Table((&models.Object{}).TableName()).Where(
 		"object_id = ? AND removed IS NOT TRUE", objectId).Find(&object).Error; err != nil {
 		return nil, err
 	}
@@ -26,7 +26,7 @@ func (db *DB) GetObject(ctx context.Context, objectId string) (*models.Object, e
 }
 
 func (db *DB) SaveObjectToSQL(ctx context.Context, object *models.Object) (string, []interface{}) {
-	stat := db.G.Session(&gorm.Session{DryRun: true}).Table((&models.Bucket{}).TableName()).Clauses(clause.OnConflict{
+	stat := db.G.Session(&gorm.Session{DryRun: true}).Table((&models.Object{}).TableName()).Clauses(clause.OnConflict{
 		Columns:   []clause.Column{{Name: "object_id"}},
 		UpdateAll: true,
 	}).Create(object).Statement
@@ -34,11 +34,11 @@ func (db *DB) SaveObjectToSQL(ctx context.Context, object *models.Object) (strin
 }
 
 func (db *DB) UpdateObjectToSQL(ctx context.Context, object *models.Object) (string, []interface{}) {
-	stat := db.G.Session(&gorm.Session{DryRun: true}).Table((&models.Bucket{}).TableName()).Where("object_id = ?", object.ObjectID).Updates(object).Statement
+	stat := db.G.Session(&gorm.Session{DryRun: true}).Table((&models.Object{}).TableName()).Where("object_id = ?", object.ObjectID).Updates(object).Statement
 	return stat.SQL.String(), stat.Vars
 }
 
 func (db *DB) UpdateObjectByBucketNameAndObjectNameToSQL(ctx context.Context, object *models.Object) (string, []interface{}) {
-	stat := db.G.Session(&gorm.Session{DryRun: true}).Table((&models.Bucket{}).TableName()).Where("bucket_name = ? AND object_name =? AND removed=false", object.BucketName, object.ObjectName).Updates(object).Statement
+	stat := db.G.Session(&gorm.Session{DryRun: true}).Table((&models.Object{}).TableName()).Where("bucket_name = ? AND object_name =? AND removed=false", object.BucketName, object.ObjectName).Updates(object).Statement
 	return stat.SQL.String(), stat.Vars
 }
