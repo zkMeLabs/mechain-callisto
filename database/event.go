3a955b23560da6b62925c70527e22cee848dc507
phenix3443 <phenix3443@gmail.com>
2024-08-27 18:24:00 +0800

    feat: update

--- database/event.go
+++ database/event.go
@@ -5,29 +5,19 @@ import (
 
 	"github.com/forbole/bdjuno/v4/database/models"
 	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 )
 
 func (db *DB) SaveBucketEventToSQL(ctx context.Context, be *models.BucketEvent) (string, []interface{}) {
-	stat := db.G.Session(&gorm.Session{DryRun: true}).Table((&models.BucketEvent{}).TableName()).Clauses(clause.OnConflict{
-		Columns:   []clause.Column{{Name: "bucket_id"}},
-		UpdateAll: true,
-	}).Create(be).Statement
+	stat := db.G.Session(&gorm.Session{DryRun: true}).Table((&models.BucketEvent{}).TableName()).Create(be).Statement
 	return stat.SQL.String(), stat.Vars
 }
 
 func (db *DB) SaveObjectEventToSQL(ctx context.Context, oe *models.ObjectEvent) (string, []interface{}) {
-	stat := db.G.Session(&gorm.Session{DryRun: true}).Table((&models.ObjectEvent{}).TableName()).Clauses(clause.OnConflict{
-		Columns:   []clause.Column{{Name: "object_id"}},
-		UpdateAll: true,
-	}).Create(oe).Statement
+	stat := db.G.Session(&gorm.Session{DryRun: true}).Table((&models.ObjectEvent{}).TableName()).Create(oe).Statement
 	return stat.SQL.String(), stat.Vars
 }
 
 func (db *DB) SaveGroupEventToSQL(ctx context.Context, ge *models.GroupEvent) (string, []interface{}) {
-	stat := db.G.Session(&gorm.Session{DryRun: true}).Table((&models.GroupEvent{}).TableName()).Clauses(clause.OnConflict{
-		Columns:   []clause.Column{{Name: "group_id"}},
-		UpdateAll: true,
-	}).Create(ge).Statement
+	stat := db.G.Session(&gorm.Session{DryRun: true}).Table((&models.GroupEvent{}).TableName()).Create(ge).Statement
 	return stat.SQL.String(), stat.Vars
 }
